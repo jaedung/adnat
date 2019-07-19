7ca3797b4fc3cc8927b115eef7c3910a7bc2b9ab
justin <justinkim0310@gmail.com>
2019-07-19 17:02:42 +1000

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
 
+  # organisations
+  get "/organisations/:id", to: "organisations#show"
+  get "/organisations/:id/edit", to: "organisations#edit"
+  post "/organisations/:id", to: "organisations#update"
+
+  resources :organisations
+
 end
