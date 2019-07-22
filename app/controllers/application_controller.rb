5be41255e57bf8a0e78bc3b61b7d6a00befccc3f
justin <justinkim0310@gmail.com>
2019-07-22 12:40:13 +1000

    disabled authenticity_token for testing purpose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  # protect_from_forgery prepend: true
-  # protect_from_forgery with: :null_session
-  # protect_from_forgery with: :exception, prepend: true
-  # protect_from_forgery prepend: true
   skip_before_action :verify_authenticity_token
 
   before_action :configure_permitted_parameters, if: :devise_controller?
